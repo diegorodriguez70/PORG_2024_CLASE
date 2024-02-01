1b55f957e2d9fdd0894f3b494e9897cfd9a6fe47
Mañana <Maana@AULA8-18>
2024-02-01 09:17:38 +0100

    atributo contador en Aula para saber cuantos alumnos hay y setter de alumnos al vector capacidad

--- src/clase/Aula.java
+++ src/clase/Aula.java
@@ -2,9 +2,14 @@ package clase;
 
 public class Aula {
 	private Alumno capacidad[];
-	
+	private int contador;
 	
 	public Aula() {
 		capacidad= new Alumno[5];
+	}
+	
+	
+	public void serAlumno(Alumno alumno) {
+		capacidad[contador]=alumno;
 	}
 }
